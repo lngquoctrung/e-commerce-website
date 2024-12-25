90055b0d2e8d25c701d4870e4ebdf41b24499ed1
QcTrung <89630327+lngquoctrung@users.noreply.github.com>
2024-12-25 16:22:47 +0700

    Delete backend directory

--- backend/auth_service/src/middlewares/authentication/refresh-token/refresh_token_middlewares.js
+++
@@ -1,7 +0,0 @@
-const decode_token = require('./decode_token');
-const generate_new_token = require('./generate_new_token');
-
-module.exports = {
-    decode_token,
-    generate_new_token,
-}
--- backend/profile-service/src/middlewares/auth/auth_middlewares.js
+++
@@ -1,7 +0,0 @@
-const check_authentication = require('./check_authentication');
-const check_authorization = require('./check_authorization');
-
-module.exports = {
-    check_authentication,
-    check_authorization,
-}
